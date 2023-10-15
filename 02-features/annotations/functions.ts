3fca59dd890fc3d3ecde612579f9b0f1cfd1548c
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-15 23:58:41 +0200

    Learned type annotation for functions

---
+++ 02-features/annotations/functions.ts
@@ -0,0 +1,36 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// Annotate with 'never' if the function truly never returns anything
+const throwError = (message: string): void => {
+  if (!message) throw new Error(message);
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({ date, weather }: { date: Date; weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
