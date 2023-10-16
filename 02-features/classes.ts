790dcfe3c9d7dda0cce74c1fa2a04b8eac3f3eba
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-16 13:22:43 +0200

    Learned about classes

---
+++ 02-features/classes.ts
@@ -0,0 +1,31 @@
+class Vehicle {
+  // color: string;
+  constructor(public color: string) {
+    // this.color = color;
+  }
+
+  protected honk(): void {
+    console.log("beep");
+  }
+}
+
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, "red");
+car.startDrivingProcess();
