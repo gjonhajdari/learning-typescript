8ccb25453210bdbffd5c38876398d1a57834018e
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-20 00:01:58 +0200

    Finished exercise 06

--- matt-pocock/react-with-typescript/src/02-components/06-typing-children.problem.tsx
+++ matt-pocock/react-with-typescript/src/02-components/06-typing-children.problem.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { PropsWithChildren, ReactNode } from "react";
 
-export const Button = (props: {}) => {
+export const Button = (props: { children: ReactNode }) => {
   return <button>{props.children}</button>;
 };
 
