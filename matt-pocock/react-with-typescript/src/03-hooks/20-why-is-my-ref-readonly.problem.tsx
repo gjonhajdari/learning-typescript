66c0326792163dae19418fe7c99cf3e21226c5c1
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-10-20 11:21:01 +0200

    Finished exercise 20

--- matt-pocock/react-with-typescript/src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
+++ matt-pocock/react-with-typescript/src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
@@ -1,8 +1,16 @@
 import { useRef } from "react";
 
 export const Component = () => {
-  const ref = useRef<string>(null);
+  const firstOverload = useRef<string>("12345");
+  firstOverload.current = "asf98hfao";
 
+  const secondOverload = useRef<string>(null); // Readonly
+  secondOverload.current = "OAJS*Ha9hfa";
+
+  const thirdOverload = useRef<string>();
+  thirdOverload.current = "shdad7h7as";
+
+  const ref = useRef<string>();
   // Why is this not allowed?
   ref.current = "Hello";
 
